199dddf7008b6c37d6a6b45934dac5cacde2a05e
そーたろう <81458517+Soh-files@users.noreply.github.com>
2021-07-08 11:42:05 +0900

    2021-07-08 modified.
    ・文字の重複可/不可の設定を追加
    ・出力される文字列よりも選択された文字数の総数が小さい場合は強制的に重複可が選択され、変更できない

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const symbol = document.getElementById('symbol');
     const Button = document.getElementById('makePWD');
     const textArea = document.getElementById('areaOutput');
+    const radioDoubleOK = document.getElementById('doubleOK');
+    const radioDoubleNG = document.getElementById('doubleNG');
+    const countWarning = document.getElementById('countWarning');
 
-    const strSelected = document.querySelectorAll('input');
+    const strSelected = document.querySelectorAll('.strSelector');
     const strOriginal = [];
     const strSetArray = [];
     const passArray = [];
@@ -23,10 +26,20 @@ document.addEventListener('DOMContentLoaded', function() {
     let strProduced = '';
     let strOutput = '';
 
-    // 選択された文字数を取得する
-    getCount.addEventListener('change', function () {
-        strLength = getCount.value;
-    });
+    // 出力される文字列の文字数と選択された文字の合計を比較
+    const compareCount = () => {
+        if (strSetOut.length < strLength) {
+            countWarning.style.display = 'block';
+            radioDoubleNG.checked = false;
+            radioDoubleOK.checked = true;
+            radioDoubleNG.setAttribute('disabled', true);
+            radioDoubleOK.setAttribute('disabled', true);
+        } else {
+            countWarning.style.display = 'none';
+            radioDoubleNG.removeAttribute('disabled');
+            radioDoubleOK.removeAttribute('disabled');
+        }
+    }
 
     // 選択された文字種類からパスワードに使用する文字群を作成する
     const makeStrSetArray = () => {
@@ -37,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         strSetOut = strSetTemp;
         strSetTemp = '';
+        compareCount();
 
         // 選択された文字種類一式を配列に変換
         strSetArray.length = 0;
@@ -45,6 +59,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    // 選択された文字数を取得する
+    getCount.addEventListener('change', function () {
+        strLength = getCount.value;
+        compareCount();
+    });
+    
     // 文字種類を取得する
     for(let i = 0; i < strSelected.length; i++) {
         strSelected[i].addEventListener('change', function () {
@@ -58,12 +78,13 @@ document.addEventListener('DOMContentLoaded', function() {
         strOutput = '';
         passArray.length = 0;
         textArea.value = '';
-        console.log(textArea.textContent);
         for (let count = 0; count < strLength; count++) {
             // strSetArrayの中からランダムで選ばれた要素を追加、追加した後要素を削除
             let t = Math.floor(Math.random() * strSetArray.length);
             passArray[count] = strSetArray[t];
-            strSetArray.splice(t, 1);
+            if(radioDoubleNG.checked) {
+                strSetArray.splice(t, 1);
+            }
         }
         for (let m = 0; m < passArray.length; m++) {
             strProduced += passArray[m];
